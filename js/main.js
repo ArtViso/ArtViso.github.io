ba4cf7ee455adca03e648895c4617f004037b45f
ArtViso <49860819+ArtViso@users.noreply.github.com>
2020-06-17 12:55:55 +0300

    Last

---
+++ js/main.js
@@ -0,0 +1,93 @@
+window.addEventListener('DOMContentLoaded', () => {
+    'use strict'
+  
+    const tabHeader = document.querySelector('.buttons'), /*Родительский элемент для кнопок*/
+          tabs = Array.from(document.querySelectorAll('.tab_button')), /*Кнопки*/
+          tabsContent = document.querySelectorAll('#tab_content') /*Контенты которые будут меняться*/
+  
+    showTab(0)
+    tabHeader.addEventListener('click', event => {
+      if(event.target && event.target.classList.contains('tab_button')) {
+        showTab(tabs.indexOf(event.target))
+      }
+    })
+
+    function showTab(index) {
+      tabsContent.forEach(item => {
+        item.classList.remove('show')
+        item.classList.add('hide')
+      })
+      tabsContent[index].classList.remove('hide')
+      tabsContent[index].classList.add('show')
+    }
+  })
+
+$('.slider-single').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: false,
+    adaptiveHeight: true,
+    infinite: false,
+   useTransform: true,
+    speed: 400,
+    cssEase: 'cubic-bezier(0.77, 0, 0.18, 1)',
+  });
+ 
+  $('.slider-nav')
+    .on('init', function(event, slick) {
+      $('.slider-nav .slick-slide.slick-current').addClass('is-active');
+    })
+    .slick({
+      slidesToShow: 7,
+      slidesToScroll: 7,
+      dots: false,
+      focusOnSelect: false,
+      infinite: false,
+      responsive: [{
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 5,
+        }
+      }, {
+        breakpoint: 640,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 4,
+       }
+      }, {
+        breakpoint: 420,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+     }
+      }]
+    });
+ 
+  $('.slider-single').on('afterChange', function(event, slick, currentSlide) {
+    $('.slider-nav').slick('slickGoTo', currentSlide);
+    var currrentNavSlideElem = '.slider-nav .slick-slide[data-slick-index="' + currentSlide + '"]';
+    $('.slider-nav .slick-slide.is-active').removeClass('is-active');
+    $(currrentNavSlideElem).addClass('is-active');
+  });
+ 
+  $('.slider-nav').on('click', '.slick-slide', function(event) {
+    event.preventDefault();
+    var goToSingleSlide = $(this).data('slick-index');
+ 
+    $('.slider-single').slick('slickGoTo', goToSingleSlide);
+  });
+  
+  jQuery(function($){
+    $('a[href*="#"]').on('click.smoothscroll', function( e ){
+    var hash    = this.hash, _hash   = hash.replace(/#/,''), theHref = $(this).attr('href').replace(/#.*/, '');
+    if( theHref && location.href.replace(/#.*/,'') != theHref ) return;
+    var $target = _hash === '' ? $('body') : $( hash + ', a[name="'+ _hash +'"]').first();
+    if( ! $target.length ) return;
+    e.preventDefault();
+    $('html, body').stop().animate({ scrollTop: $target.offset().top - 0 }, 400, 'swing', function(){
+    window.location.hash = hash;
+    });
+    });
+    });
